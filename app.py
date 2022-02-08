27a27f3efca74e31541b7356e46c6066af16302a
kajarenc <kajarenc@gmail.com>
2022-02-08 16:45:15 +0400

    update app texts

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ st.title("Hello World")
 with st.echo():
     st.write(st.user)
     st.write(st.user.email)
+
+
+if st.user.email:
+    st.write(
+        "You are not logged in :( Please log in with SSO to see your email address.")
+else:
+    st.write(f"You are logged in as {st.user.email}!")
+
+if st.user.email == "ben@streamlit.io":
+    st.write("You are logged in as Ben!")
+    st.balloons()
