82cb95c5b7f4c367285308159320dbcedf84ec14
kajarenc <kajarenc@gmail.com>
2022-02-08 16:51:08 +0400

    update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ with st.echo():
     st.write(st.user.email)
 
 
-if st.user.email:
+if not st.user.email:
     st.write(
         "You are not logged in :( Please log in with SSO to see your email address.")
 else:
