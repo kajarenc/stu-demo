ce6a8ffdba616b542904fe8da99c3ffa2fd0eb0b
kajarenc <kajarenc@gmail.com>
2022-02-10 22:24:42 +0400

    final fixes

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("Hello st.user")
 
 with st.echo():
     st.write(st.user)
     st.write(st.user.email)
 
-
-if not st.user.email:
-    st.write(
-        "You are not logged in :( Please log in with Google or SSO to see your email address.")
-else:
-    st.write(f"You are logged in as {st.user.email}!")
+    if not st.user.email:
+        st.write(
+            "You are not logged in :( Please log in with Google or SSO to see your email address.")
+    else:
+        st.write(f"You are logged in as {st.user.email}!")
